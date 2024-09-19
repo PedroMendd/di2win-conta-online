c13035a067a7037158bd830f71e89bcb91efa5a7
Pedro Mendonça <althander@gmail.com>
2024-09-19 17:03:08 -0300

    Atualização de Testes finais para empacotamente e adicição de docker

--- src/test/java/com/di2win/contaonline/Di2winContaOnlineApplicationTests.java
+++ src/test/java/com/di2win/contaonline/Di2winContaOnlineApplicationTests.java
@@ -2,7 +2,9 @@ package com.di2win.contaonline;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
+@ActiveProfiles("test")
 @SpringBootTest
 class Di2winContaOnlineApplicationTests {
 
